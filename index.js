fa9c74c2a714d30db33bf829afcb2d6c3a78a2c7
moehzi <faizi1234098@gmail.com>
2021-10-12 12:20:40 +0800

    feat: create server with express

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const path = require("path");
+app.use("/assets", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${path.join(__dirname)}/views/index.html`);
+});
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
